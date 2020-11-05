2cd15b6efe684e1c3adcf1e598091abdec40b930
andrewlee29 <andrewlee201.al@gmail.com>
2020-11-05 18:32:42 -0500

    test

--- create_database.py
+++ create_database.py
@@ -22,14 +22,22 @@ mycursor = mydb.cursor()
 mycursor.execute("CREATE TABLE IF NOT EXISTS summarydata (cid INT AUTO_INCREMENT PRIMARY KEY, date VARCHAR(255), environmentStatus VARCHAR(255) , muscleStatus VARCHAR(255))")
 mycursor.execute("CREATE TABLE IF NOT EXISTS sensordata (sid INT AUTO_INCREMENT PRIMARY KEY, time VARCHAR(255), mV VARCHAR(255) , emgsensor VARCHAR(255) , temperature VARCHAR(255) , humidity VARCHAR(255) , cid INT, FOREIGN KEY(cid) REFERENCES summarydata(cid))")
 
-# insert data
+### insert data
 
 # ycursor.execute("INSERT INTO summarydata (date, environmentStatus, muscleStatus) VALUES (%s,%s,%s)", ("2020/11/5", "good", "good"))
 ### automatic generate id 
 # cid = mycursor.lastrowid
-mycursor.execute("INSERT INTO sensordata (time, mV, emgsensor, temperature, humidity, cid) VALUES (%s,%s,%s,%s,%s,%s)", ("1", "50", "1", "70", "30",1))
-sid = mycursor.lastrowid
+#mycursor.execute("INSERT INTO sensordata (time, mV, emgsensor, temperature, humidity, cid) VALUES (%s,%s,%s,%s,%s,%s)", ("1", "50", "1", "70", "30",1))
+#sid = mycursor.lastrowid
 
+### select data and print 
+mycursor.execute("SELECT * FROM summarydata")
+### get the lastest record
+# mycursor.execute("SELECT * FROM summarydata WHERE cid=(SELECT max(cid) FROM summarydata)")
+myresult = mycursor.fetchall()
+
+for x in myresult:
+  print(x)
 
 
 mydb.commit()
