e7601445c42765a2f6c827e01c9fe7d4a472fabe
andrewlee29 <andrewlee201.al@gmail.com>
2020-11-05 18:02:33 -0500

    test

--- create_database.py
+++ create_database.py
@@ -1,5 +1,6 @@
 import mysql.connector
 
+# create database
 mydb = mysql.connector.connect(
   host="localhost",
   user="root",
@@ -20,3 +21,11 @@ mydb = mysql.connector.connect(
 mycursor = mydb.cursor()
 mycursor.execute("CREATE TABLE IF NOT EXISTS summarydata (cid INT AUTO_INCREMENT PRIMARY KEY, date VARCHAR(255), environmentStatus VARCHAR(255) , muscleStatus VARCHAR(255))")
 mycursor.execute("CREATE TABLE IF NOT EXISTS sensordata (sid INT AUTO_INCREMENT PRIMARY KEY, time VARCHAR(255), mV VARCHAR(255) , emgsensor VARCHAR(255) , temperature VARCHAR(255) , humidity VARCHAR(255) , cid INT, FOREIGN KEY(cid) REFERENCES summarydata(cid))")
+
+# insert data
+
+summary = "INSERT INTO summarydata (cid, date, environmentStatus, muscleStatus) VALUES (?,?,?,?)"
+val = (1, "2020/11/5", "good", "good")
+mycursor.execute(summary, val)
+
+mydb.commit()
