4cfbc867b0ec83b14886760e48c4d34a27b2eff2
andrewlee29 <andrewlee201.al@gmail.com>
2020-11-09 15:28:45 -0500

    test

---
+++ InitDatabase.py
@@ -0,0 +1,34 @@
+import mysql.connector
+
+### create database
+def setupdatabase():
+  mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="123456"
+  )
+  mycursor = mydb.cursor()
+  mycursor.execute("CREATE DATABASE IF NOT EXISTS prostheticsData")
+  mydb.close()
+  ### open database 
+  mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="123456",
+    database= "prostheticsData"
+  )
+  mycursor = mydb.cursor()
+  mycursor.execute("CREATE TABLE IF NOT EXISTS summarydata (cid INT AUTO_INCREMENT PRIMARY KEY, date VARCHAR(255), environmentStatus VARCHAR(255) , muscleStatus VARCHAR(255))")
+  mycursor.execute("CREATE TABLE IF NOT EXISTS sensordata (sid INT AUTO_INCREMENT PRIMARY KEY, time VARCHAR(255), mV VARCHAR(255) , emgsensor VARCHAR(255) , temperature VARCHAR(255) , humidity VARCHAR(255) , cid INT, FOREIGN KEY(cid) REFERENCES summarydata(cid))")
+  mydb.close()
+
+
+### insert data
+
+# ycursor.execute("INSERT INTO summarydata (date, environmentStatus, muscleStatus) VALUES (%s,%s,%s)", ("2020/11/5", "good", "good"))
+### automatic generate id 
+# cid = mycursor.lastrowid
+#mycursor.execute("INSERT INTO sensordata (time, mV, emgsensor, temperature, humidity, cid) VALUES (%s,%s,%s,%s,%s,%s)", ("1", "50", "1", "70", "30",1))
+#sid = mycursor.lastrowid
+### need commit to apply store 
+# mydb.commit()
--- loadsqldata.py
+++ loadsqldata.py
@@ -27,9 +27,27 @@ class loadData:
             message += ele
             message += "|"
         return message
+
+    def getHistList(self):
+        self.mycursor.execute("SELECT summarydata.date as Date, AVG(sensordata.temperture) FROM summarydata JOIN sensordata as s ON summarydata.cid = sensordata.cid GROUP BY s.cid")
+        data = self.mycursor.fetchall()
+        currsum = []
+        message = ""
+        for row in data:
+            # Date
+            currsum.append(row[1])
+            # environmentStatus
+            currsum.append(row[2])
+            # muscleStatus
+            currsum.append(row[3])
+            # return string
+        for ele in currsum:
+            message += ele
+            message += "|"
+        return message
     
 
-# ### open database 
-# loaddata = loadData()
-# x = loaddata.getCurrentSum()
-# print(x)
+### open database 
+loaddata = loadData()
+x = loaddata.getCurrentSum()
+print(x)
