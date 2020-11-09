7fb06bd56b993a8520caf7415386d6dcb8cae589
andrewlee29 <andrewlee201.al@gmail.com>
2020-11-09 12:15:35 -0500

    test

--- loadsqldata.py
+++ loadsqldata.py
@@ -2,48 +2,21 @@ import mysql.connector
 
 class loadData:
     def _init_(self):
-        self.currsum = []
-        self.currdata = []
-        self.historylist = []
-        self.historyinfo = []
         self.message = ""
     def getCurrentSum(self):
         mycursor.execute("SELECT * FROM summarydata WHERE cid=(SELECT max(cid) FROM summarydata)")
         data = mycursor.fetchall()
+        currsum = []
         for row in data:
             # environmentStatus
-            self.currsum.append(row[2])
+            currsum.append(row[2])
             # muscleStatus
-            self.currsum.append(row[3])
+            currsum.append(row[3])
             # return string
-            for ele in self.currsum:
+            for ele in currsum:
                 self.message += ele
                 self.message += "|"
             return self.message
-    def getCurrentData(self):
-        mycursor.execute("SELECT * FROM summarydata WHERE cid=(SELECT max(cid) FROM summarydata)")
-        data = mycursor.fetchall()
-        for row in data:
-            # environmentStatus
-            self.currsum.append(row[2])
-            # muscleStatus
-            self.currsum.append(row[3])
-    def gethistorylist(self):
-        mycursor.execute("SELECT * FROM summarydata WHERE cid=(SELECT max(cid) FROM summarydata)")
-        data = mycursor.fetchall()
-        for row in data:
-            # environmentStatus
-            self.currsum.append(row[2])
-            # muscleStatus
-            self.currsum.append(row[3])
-    def gethistoryinfo(self):
-        mycursor.execute("SELECT * FROM summarydata WHERE cid=(SELECT max(cid) FROM summarydata)")
-        data = mycursor.fetchall()
-        for row in data:
-            # environmentStatus
-            self.currsum.append(row[2])
-            # muscleStatus
-            self.currsum.append(row[3])
 
 ### open database 
 mydb = mysql.connector.connect(
