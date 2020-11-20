48f1abc8dbc7970659ce9a210dcb95b748c30d16
andrewlee29 <andrewlee201.al@gmail.com>
2020-11-20 16:26:44 -0500

    test

--- loadsqldata.py
+++ loadsqldata.py
@@ -129,7 +129,7 @@ class loadData:
         #     message += "#"
         
         # load all possible section to the data
-        self.mycursor.execute("SELECT section FROM sensordata WHERE cid= " +str(cid)+ " AND section= "+ str(getsection)+" GROUP BY section")
+        self.mycursor.execute("SELECT section FROM sensordata WHERE cid= " +str(cid)+" GROUP BY section")
         data = self.mycursor.fetchall()
         y = {"section":data[0]}
         x.update(y)
